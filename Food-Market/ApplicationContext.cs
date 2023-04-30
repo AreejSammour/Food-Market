c4608c37500ad194622767191b498db05d5ff77e
amandajonort <amandajonort@gmail.com>
2023-04-30 11:10:28 +0200

    First commit, main project
    Amanda har installerat dessa NuGet Packages
    Microsoft.EntityFrameworkCore.SqlServer
    Microsoft.EntityFrameworkCore.Design
    Microsoft.EntityFrameworkCore.Tools
    Microsoft.EntityFrameworkCore
    System.Data.SqlClient
    
    Fixat ApplicationContext.cs, builder.AddDbContext i Program.cs och lagt till ConnectionString i appsettings.json.

---
+++ Food-Market/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Food_Market
+{
+	public class ApplicationContext : DbContext
+	{
+        //public DbSet<Product> Products { get; set; }
+        //public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        //public DbSet<CartItem> CartItems { get; set; }
+
+        //public DbSet<User> Users { get; set; }
+        //public DbSet<Admin> Admins { get; set; }
+        //public DbSet<Ticket> Tickets { get; set; }
+
+        //Constructor
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+    }
+}
+
