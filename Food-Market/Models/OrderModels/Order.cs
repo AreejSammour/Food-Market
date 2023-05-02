129a39d68b217a0201c7cb0a5a3e1bbbc9cdf95b
Asammour <areej_sammour@hotmail.com>
2023-05-02 13:56:41 +0200

    AddOrderModels

---
+++ Food-Market/Models/OrderModels/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Food_Market.Models.OrderModels
+{
+	public class Order
+	{
+		private string userEmail;
+
+		public int OrderId { get; set; }
+		[Required]
+		public string UserEmail { get => userEmail; set => userEmail = value; }
+		public DateTime CreatedDate { get; set; } = DateTime.Now;
+		public decimal OrderTotal { get; set; }
+		public string OrderStatus { get; set; }
+	}
+}
