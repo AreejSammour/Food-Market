9dbde72bc20947dc2f21045c7d7a964c98099c42
henke00 <henrik.stenberg00@gmail.com>
2023-05-02 12:56:11 +0200

    Henriks Models

---
+++ Food-Market/Models/Checkout.cs
@@ -0,0 +1,22 @@
+﻿namespace Ecommerce_assignment.Models
+{
+    public class Checkout
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string CardNumber { get; set; }
+        public string ExpirationDate { get; set; }
+        public string Cvc { get; set; }
+
+        public void Validate()
+        {
+            // Validate user input for name, address, and card details
+        }
+
+        //public void ProcessOrder(List<Product> products)
+        //{
+        //    // Create a new Order object based on the user input and the products in the cart
+        //}
+    }
+
+}
