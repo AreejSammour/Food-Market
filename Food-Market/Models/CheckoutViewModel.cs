a8b05ea7c1e124a031c5e32c4e97ace0dcc1c9e7
moawia-dev <moawia.asali@gmail.com>
2023-05-09 11:39:10 +0200

    Final before order

--- Food-Market/Models/CheckoutViewModel.cs
+++ Food-Market/Models/CheckoutViewModel.cs
@@ -1,20 +1,65 @@
-﻿namespace Ecommerce_assignment.Models
+﻿using Food_Market.Models.ShoppingCart;
+using System.ComponentModel.DataAnnotations;
+
+namusing Food_Market.Models.ShoppingCart;
+using System.ComponentModel.DataAnnotations;
+
+namespace Food_Market.Models
 {
     public class CheckoutViewModel
     {
-        public string Name { get; set; }
+        public List<CartItem> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+
+        [Required(ErrorMessage = "Please enter your first name")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your last name")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Please enter your address")]
+        [Display(Name = "Address")]
         public string Address { get; set; }
+
+        [Required(ErrorMessage = "Please enter your city")]
+        [Display(Name = "City")]
         public string City { get; set; }
-        public string PostalCode { get; set; }
+
+        [Required(ErrorMessage = "Please enter your state")]
+        [Display(Name = "State")]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "Please enter your country")]
+        [Display(Name = "Country")]
         public string Country { get; set; }
 
+        [Required(ErrorMessage = "Please enter your zip code")]
+        [DataType(DataType.PostalCode)]
+        [Display(Name = "Zip Code")]
+        public string ZipCode { get; set; }
+
+
+        [DataType(DataType.CreditCard)]
+        public string CardNumber { get; set; }
+        public string NameOnCard { get; set; }
+        public int CVV { get; set; }
         public decimal Subtotal { get; set; }
         public decimal ShippingRate { get; set; }
         public decimal Total { get; set; }
 
         public string Currency { get; set; }
-
-        //public List<CartItemViewModel> CartItems { get; set; }
     }
 
 }
