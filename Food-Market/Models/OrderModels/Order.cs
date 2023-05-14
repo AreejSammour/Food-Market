6f6fa313685f32dbfadbb168bf2a4d0652fd8cad
Asammour <areej_sammour@hotmail.com>
2023-05-14 17:03:56 +0200

    Final Version

--- Food-Market/Models/OrderModels/Order.cs
+++ Food-Market/Models/OrderModels/Order.cs
@@ -1,16 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
+
 namespace Food_Market.Models.OrderModels
 {
 	public class Order
 	{
-		private string userEmail;
-
 		public int OrderId { get; set; }
-		[Required]
-		public string UserEmail { get => userEmail; set => userEmail = value; }
-		public DateTime CreatedDate { get; set; } = DateTime.Now;
-		public decimal OrderTotal { get; set; }
-		public string OrderStatus { get; set; }
+		public List<OrderDetail> OrderItems { get; set; }
+
 	}
 }
